2107e8489befddb27d4f84133653121d18537803
John Hawthorn <john@hawthorn.email>
2022-05-05 22:46:31 -0700

    Compiling asmjit

--- ext/asmjit/asmjit.cc
+++ ext/asmjit/asmjit.cc
@@ -1,9 +1,29 @@
+#include <asmjit/asmjit.h>
+
 #include "asmjit.h"
 
+using namespace asmjit;
+
 VALUE rb_mAsmjit;
+
+static JitRuntime *jit_runtime;
+
+void foo_free(void *) {}
+
+static const rb_data_type_t foo_type = {
+    .wrap_struct_name = "foo",
+    .function = {
+        .dmark = NULL,
+        .dfree = foo_free,
+        .dsize = NULL,
+    },
+    .data = NULL,
+    .flags = RUBY_TYPED_FREE_IMMEDIATELY,
+};
 
 extern "C" void
 Init_asmjit(void)
 {
-  rb_mAsmjit = rb_define_module("AsmJIT");
+    jit_runtime = new JitRuntime();
+    rb_mAsmjit = rb_define_module("AsmJIT");
 }
--- ext/asmjit/extconf.rb
+++ ext/asmjit/extconf.rb
@@ -2,4 +2,16 @@
 
 require "mkmf"
 
+asmjit_dir = File.expand_path("asmjit/src/", __dir__)
+
+$INCFLAGS << " -I#{asmjit_dir} "
+
+$CXXFLAGS << " -DASMJIT_EMBED "
+
+$srcs = []
+$srcs.concat Dir[File.join(__dir__, "*.cc")]
+$srcs.concat Dir[File.join(asmjit_dir, "**/*.cpp")]
+
+$objs = $srcs.map{|x| x.gsub(/\.(cc|cpp)\z/, ".o") }
+
 create_makefile("asmjit/asmjit")
